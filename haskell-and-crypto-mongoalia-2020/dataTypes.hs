922a5ba720e7889beb16c438875c9a047f2e22c2
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-06-12 15:16:33 -0700

    implement ||

--- haskell-and-crypto-mongoalia-2020/dataTypes.hs
+++ haskell-and-crypto-mongoalia-2020/dataTypes.hs
@@ -3,3 +3,8 @@ data Bool' = True' | False' deriving (Show)
 not' :: Bool' -> Bool'
 not' True' = False'
 not' False' = True'
+
+-- Since there are overlaps with Prelude "||" you can acccess this with "Main.||" in ghci
+(||) :: Bool -> Bool -> Bool
+False || c = c
+True || _ = True
